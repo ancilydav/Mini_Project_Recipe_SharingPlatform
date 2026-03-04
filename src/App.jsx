f23138ea3347eb7ce72f84dae6508077740ce6dc
Ancily Davis <ancily18@gmail.com>
2026-03-04 23:09:08 +0530

    Navbar created

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { Routes,Route } from 'react-router-dom'
+import Navbar from './Components/Navbar'
 import Home from './Pages/Home'
 import RecipeDetails from './Pages/Recipedetails'
 import Favourites from './Pages/Favourites'
--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,8 +1,17 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <div className='flex bg-orange-500 justify-between p-3 text-white'>
+      <h1 className=' font-bold text-xl'>FlavourLoop</h1>
+      <div className='space-x-4 text-l'>
+        <Link to="/">Home</Link>
+        <Link to="/recipe">RecipeDetails</Link>
+        <Link to="/favourites">Favourites</Link>
+      </div>
+
+    </div>
   )
 }
 
