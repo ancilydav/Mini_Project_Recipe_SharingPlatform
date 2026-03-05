211d47f7883debeaf9d92e129f7c81fd4b41d9d5
Ancily Davis <ancily18@gmail.com>
2026-03-05 14:47:48 +0530

    recipeSlice created

---
+++ src/Features/recipeSlice.js
@@ -0,0 +1,13 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState={
+
+};
+
+const recipeSlice = createSlice({
+    name:"recipe",
+    initialState,
+    reducers:{}
+});
+
+export default recipeSlice.reducer;
